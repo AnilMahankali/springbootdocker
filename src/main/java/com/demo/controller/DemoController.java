b76a1a063f0b389267602412464ed4bf56a47291
AnilKumar-Mahankali <anilk8009@gmail.com>
2020-04-22 16:10:57 +0530

    initial commit

---
+++ src/main/java/com/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+	@RequestMapping("/getMessage")
+	public String getMessage() {
+		return "Hello World!";
+	}
+}
